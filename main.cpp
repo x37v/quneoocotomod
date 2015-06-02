5037cc67d485ca3a4da8a5d5fb6afed8c357c0a6
Alex Norman <x37v.alex@gmail.com>
2015-06-01 21:30:43 -0700

    coarse and fine offsets moved into class.. showing led output

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@ const uint8_t nrpn_chan = 15;
 const uint16_t nrpn_center = (1 << 13);
 
 std::shared_ptr<midicpp::Output> midiout;
+std::shared_ptr<midicpp::Output> ledout;
 
 void sighandler(int sig) {
   should_exit = true;
@@ -51,30 +52,46 @@ class pad_note_t {
   public:
     pad_note_t() {
       for (int i = 0; i < 2; i++) {
-        mCoarse.push_back(nrpn_center + nrpn_center / 2);
-        mFine.push_back(0);
+        mCoarse.push_back(64);
+        mFine.push_back(64);
       }
     }
     uint16_t value(uint8_t index) const {
-      return mCoarse[index] + mFine[index];
+      return static_cast<uint16_t>(mCoarse[index]) * 130 + 2 * (static_cast<int16_t>(mFine[index]) - 64);
     }
-    void coarse(uint8_t index, uint16_t v) { mCoarse[index] = v; }
-    void fine(uint8_t index, int16_t v) { mFine[index] = v; }
+    void coarse(uint8_t index, uint8_t v) { mCoarse[index] = v; }
+    void fine(uint8_t index, uint8_t v) { mFine[index] = v; }
 
-    uint16_t coarse(uint8_t index) const { return mCoarse[index]; }
-    int16_t fine(uint8_t index) const { return mFine[index]; }
+    uint8_t coarse(uint8_t index) const { return mCoarse[index]; }
+    uint8_t fine(uint8_t index) const { return mFine[index]; }
 
     unsigned int num_notes() { return mCoarse.size(); }
   private:
-    std::vector<uint16_t> mCoarse;
-    std::vector<int16_t> mFine;
+    std::vector<uint8_t> mCoarse;
+    std::vector<uint8_t> mFine;
 };
 
 std::vector<pad_note_t> pad_notes(16);
 
+void light_pad(int pad_num) {
+  for (int i = 0; i < 32; i++)
+    ledout->note(false, 0, i, 127);
+
+  int led_num = pad_rm(pad_num) * 2;
+  ledout->note(true, 0, led_num, 127);
+}
+
 void send_note(int pad_num) {
+  light_pad(pad_num);
   midiout->nrpn(nrpn_chan, 0, pad_notes[pad_num].value(0));
   midiout->nrpn(nrpn_chan, 1, pad_notes[pad_num].value(1));
+
+
+  //sliders
+  ledout->cc(0, 11, pad_notes[pad_num].coarse(0));
+  ledout->cc(0, 10, pad_notes[pad_num].fine(0));
+  ledout->cc(0, 9, pad_notes[pad_num].coarse(1));
+  ledout->cc(0, 8, pad_notes[pad_num].fine(1));
 }
 
 void pad(pad_t trig, int num, int val) {
@@ -85,6 +102,9 @@ void pad(pad_t trig, int num, int val) {
       last_pad = num;
       send_note(num);
       midiout->nrpn(nrpn_chan, 7, nrpn_center + nrpn_center * (val / 127.0));
+      break;
+    case P_NOTE_OFF:
+      light_pad(last_pad);
       break;
     case P_PRESSURE:
       midiout->nrpn(nrpn_chan, 6, nrpn_center + nrpn_center * (val / 127.0));
@@ -115,8 +135,8 @@ void read_settings(std::string path) {
   if (yaml["pads"]) {
     for (int i = 0; i < std::min(yaml["pads"].size(), pad_notes.size()); i++) {
       for (int j = 0; j < yaml["pads"][i].size(); j++) {
-        pad_notes[i].fine(yaml["pads"][i][j]["fine"].as<int16_t>());
-        pad_notes[i].coarse(yaml["pads"][i][j]["coarse"].as<uint16_t>());
+        pad_notes[i].fine(j, yaml["pads"][i][j]["fine"].as<int>());
+        pad_notes[i].coarse(j, yaml["pads"][i][j]["coarse"].as<int>());
       }
     }
   }
@@ -131,8 +151,8 @@ void write_settings(std::string path) {
     YAML::Node notes;
     for (int i = 0; i < p.num_notes(); i++) {
       YAML::Node n;
-      n["fine"] = p.fine(i);
-      n["coarse"] = p.coarse(i);
+      n["fine"] = static_cast<int>(p.fine(i));
+      n["coarse"] = static_cast<int>(p.coarse(i));
       notes.push_back(n);
     }
     pads.push_back(notes);
@@ -161,9 +181,9 @@ void cc_cb(uint8_t chan, uint8_t num, uint8_t val) {
     return;
   } else if (num < 4) {
     if (num % 2 == 0) {
-      pad_notes[last_pad].coarse(num / 2, static_cast<uint16_t>(val) * 130);
+      pad_notes[last_pad].coarse(num / 2, val);
     } else {
-      pad_notes[last_pad].fine(num / 2, (static_cast<int16_t>(val) - 64) * 2);
+      pad_notes[last_pad].fine(num / 2, val);
     }
     send_note(last_pad);
   }
@@ -188,13 +208,14 @@ int main(int argc, char *argv[]) {
   
   signal(SIGINT, &sighandler);
 
-  std::string quneo("QUNEO");
+  std::string quneo_in("QUNEO");
+  std::string quneo_out("QUNEO");
   std::string xnormidi("xnormidi");
 
   cout << "inputs: " << midicpp::Input::device_count() << endl;
   for (std::string n: midicpp::Input::device_list()) {
     if (n.find("QUNEO") != std::string::npos)
-      quneo = n;
+      quneo_in = n;
     cout << "\t" << n << endl;
   }
 
@@ -202,14 +223,14 @@ int main(int argc, char *argv[]) {
   for (std::string n: midicpp::Output::device_list()) {
     if (n.find("xnormidi") != std::string::npos)
       xnormidi = n;
+    if (n.find("QUNEO") != std::string::npos)
+      quneo_out = n;
     cout << "\t" << n << endl;
   }
 
-  for (int i = 0; i < 16; i++) {
-    pad_notes[i].coarse(i * 1000 + nrpn_center);
-  }
+  midicpp::Input in(quneo_in);
+  ledout = std::make_shared<midicpp::Output>(quneo_out);
 
-  midicpp::Input in(quneo);
   midiout = std::make_shared<midicpp::Output>(xnormidi);
 
   in.with_message3(midicpp::CC, cc_cb);
